8494b8a84fcc09091920572cf8c14f8acabb904c
Szobonya Norbert <159431750+szobonyan@users.noreply.github.com>
2025-06-01 22:07:06 +0200

    Main class pdate

--- src/main/java/ticketing/Main.java
+++ src/main/java/ticketing/Main.java
@@ -1,7 +1,24 @@
 package ticketing;
+import ticketing.Database.DatabaseConnection;
+import java.sql.Connection;
+import ticketing.Database.DatabaseConnection;
+import ticketing.GUI.LoginPage;
+import ticketing.Utils.LoggerUtil;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            System.out.println(" Sikeres kapcsolat az adatbázishoz!");
+            DatabaseConnection.closeConnection();
+        } catch (Exception e) {
+            System.err.println("Sikertelen kapcsolat: " + e.getMessage());
+            e.printStackTrace();
+        }
 
+        new LoginPage();
+        LoggerUtil.log("Alkalmazás elindult.");
     }
 }
